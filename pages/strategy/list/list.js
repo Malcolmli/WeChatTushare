fa6dda6e5b876ffce315e16d7eb3e786cd862d7a
Malcolm <MalcolmSelf@Outlook.com>
2020-09-10 21:28:14 +0800

    4.0

--- pages/strategy/list/list.js
+++ pages/strategy/list/list.js
@@ -2,8 +2,13 @@
 Page({
 
   data: {
-    info1: "",
-    info2: "",
+    query:"",
+    query1:"",
+    query2:"",
+    query3:"",
+    query4:"",
+    winRate: "",
+    annualYield: "",
     select: "",
     array: [],
   },
@@ -15,13 +20,16 @@ Page({
         console.log(res.data)
         self.setData({
           array: res.data.historyDetails,
-          info1: res.data.reportData.winRate,
-          info2: res.data.reportData.annualYield,
+          query: res.data.apiParam.query,
+          query1: res.data.apiParam.period,
+          query2: res.data.apiParam.upper_income,
+          query3: res.data.apiParam.lower_income,
+          query4: res.data.apiParam.fall_income,
+          winRate: res.data.reportData.winRate,
+          annualYield: res.data.reportData.annualYield,
           select: res.data.pickData.stock
         })
       }
     })
   }
-
-
 })
