fcdf3763a0557cd31f189de5113937146667dd9f
Malcolm <MalcolmSelf@outlook.com>
2020-10-23 09:24:32 +0800

    .0

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,105 @@
-// pages/index.js
+const util = require('../utils/util.js')
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    limit: 6,
 
+    dateValue: "",
+
+    serachType: ["均幅", "胜率"],
+    serachTypeValues: ["range", "ratio"],
+    serachTypeIndex: 0,
+    searchShow: true,
+
+    serachTarge: ["开高", "开闭", "低高", "低闭"],
+    serachTargeValues: ["oh", "oc", "lh", "lc"],
+    serachTargeIndex: 0,
+
+    rules: [{
+      name: 'limit',
+      rules: {
+        required: true,
+        message: '限制 不能为空!'
+      }
+    }],
+    formData: {
+
+    },
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
-
+    var url='https://www.malcolmli.cn:8050/tool/latestData'
+    util.requestPromise(url).then(res => {
+      this.setData({
+        dateValue: res.data
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  bindDateChange: function (e) {
+    this.setData({
+      dateValue: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  formInputChange(e) {
+    const {
+      field
+    } = e.currentTarget.dataset
+    this.setData({
+      [`formData.${field}`]: e.detail.value,
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  bindTypeChange: function (e) {
+    this.setData({
+      serachTypeIndex: e.detail.value
+    })
+    if (e.detail.value == 0) {
+      this.setData({
+        limit: 6
+      })
+    }
+    if (e.detail.value == 1) {
+      this.setData({
+        limit: 0.8
+      })
+    }
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  bindTargeChange: function (e) {
+    this.setData({
+      serachTargeIndex: e.detail.value
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  searchbtn: function (e) {
+    var type = this.data.serachTypeValues[this.data.serachTypeIndex]
+    var targe = this.data.serachTargeValues[this.data.serachTargeIndex]
+    var date = this.data.dateValue.replace(/-/g,'');
+    var limit = this.data.limit;
+    this.requestRange(type, targe, date, limit);
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  requestRange: function (type, targe, date, limit) {
+    var url = "https://www.malcolmli.cn:8050/" + type + "/" + targe + "?date=" + date + "&limit=" + limit
+    console.log(url)
+    this.setData({
+      searchShow: false,
+    })
+    util.requestPromise(url)
+      .then(res => {
+        this.setData({
+          searchShow: true,
+        })
+        if (res.data.errorMsg != null) {
+          wx.showToast({
+            title: res.data.errorMsg,
+            icon: 'none',
+          })
+        } else {
+          console.log(res.data.data)
+          wx.setStorage({
+            key: "listDate",
+            data: res.data.data
+          })
+          wx.navigateTo({
+            url: "list/list"
+          })
+        }
+      })
   }
 })
